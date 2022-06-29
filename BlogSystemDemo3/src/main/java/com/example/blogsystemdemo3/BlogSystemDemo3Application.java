9b43beed580d7e1edf169242ca462849f7dc7a89
谢知则 <72195062+DTNightWatchman@users.noreply.github.com>
2022-06-29 15:39:22 +0800

    博客demo3

---
+++ BlogSystemDemo3/src/main/java/com/example/blogsystemdemo3/BlogSystemDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.blogsystemdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSystemDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSystemDemo3Application.class, args);
+    }
+
+}
