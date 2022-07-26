f5ec43935e01d8b53056b36395eb9cc3768d2757
lenovo <2975545458@qq.com>
2022-07-26 11:12:46 +0800

    简单java在线oj

---
+++ demo_java_oj/src/main/java/com/example/demo_java_oj/DemoJavaOjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_java_oj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaOjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJavaOjApplication.class, args);
+    }
+
+}
